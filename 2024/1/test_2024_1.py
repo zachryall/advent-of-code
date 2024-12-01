a8761e5665eef08bdc4227d17f26589b5ba279e3
Zach Ryall <zach.ryall16@gmail.com>
2024-12-01 13:58:16 +0000

    improve tests

--- 2024/1/test_2024_1.py
+++ 2024/1/test_2024_1.py
@@ -4,13 +4,12 @@
 from a import ANSWER as answer_a
 from b import ANSWER as answer_b
 
-def test_check_part_a_is_correct():
+def test_part_a_answer_is_correct():
     """Test to check that part A gets the correct answer
     """
     assert answer_a == 1830467
 
-def test_check_part_b_is_correct():
+def test_part_b_answer_is_correct():
     """Test to check that part B gets the correct answer
     """
     assert answer_b == 26674158
-
