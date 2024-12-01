095c9fa2a9f54af6037b39f4d8c0fdc9a52d65bf
Zach Ryall <zach.ryall16@gmail.com>
2024-12-01 13:51:00 +0000

    add unit tests

---
+++ 2024/1/test_2024_1.py
@@ -0,0 +1,16 @@
+"""Advent of Code 2024 Day 1 Tests
+"""
+
+from a import ANSWER as answer_a
+from b import ANSWER as answer_b
+
+def test_check_part_a_is_correct():
+    """Test to check that part A gets the correct answer
+    """
+    assert answer_a == 1830467
+
+def test_check_part_b_is_correct():
+    """Test to check that part B gets the correct answer
+    """
+    assert answer_b == 26674158
+
