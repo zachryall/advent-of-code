4e77110e1d16479b0fc58e8b14ea723d7b1a2608
zachryall <zach.ryall16@gmail.com>
2024-12-01 15:20:53 +0000

    2024 - Day 1 unit tests (#4)

--- 2024/1/b.py
+++ 2024/1/b.py
@@ -1,19 +1,33 @@
 """
-Advent of Code 2024 - Day 1b
+Advent of Code 2024 - Day 1b - 26674158
 """
 
-idsA = []
-idsB = []
-TOTAL = 0
+from pathlib import Path
 
-with open('input.txt', 'r', encoding='UTF-8') as file:
-    lines = file.readlines()
-    for line in lines:
-        row = line.rstrip().split('   ')
-        idsA.append(int(row[0]))
-        idsB.append(int(row[1]))
+def main(file_name):
+    """Main function
 
-for content in idsA:
-    TOTAL += content * (idsB.count(content))
+    :param file_name: The file that contexts the puzzle input
+    """
+    ids_a = []
+    ids_b = []
+    answer = 0
 
-print(TOTAL)
+    path = Path(__file__).with_name(file_name)
+    with path.open('r', encoding='UTF-8') as file:
+        lines = file.readlines()
+        for line in lines:
+            row = line.rstrip().split('   ')
+            ids_a.append(int(row[0]))
+            ids_b.append(int(row[1]))
+
+    for content in ids_a:
+        answer += content * (ids_b.count(content))
+
+    return answer
+
+answer_example = main('example.txt')
+answer_input = main('input.txt')
+
+print(f'Example Input: {answer_example}')
+print(f'Puzzle Input: {answer_input}')
---
+++ 2024/1/test_2024_1.py
@@ -0,0 +1,25 @@
+"""Advent of Code 2024 Day 1 Tests
+"""
+
+from a import answer_example as a_example, answer_input as a_input
+from b import answer_example as b_example, answer_input as b_input
+
+def test_part_a_example_is_correct():
+    """Test to check that part A's example gets the correct answer
+    """
+    assert a_example == 11
+
+def test_part_a_answer_is_correct():
+    """Test to check that part A's input gets the correct answer
+    """
+    assert a_input == 1830478
+
+def test_part_b_example_is_correct():
+    """Test to check that part A's example gets the correct answer
+    """
+    assert b_example == 31
+
+def test_part_b_answer_is_correct():
+    """Test to check that part A's input gets the correct answer
+    """
+    assert b_input == 26674158
