383bf5a803548eebf7df6627aa645ae0557b3f37
Zach Ryall <zach.ryall16@gmail.com>
2024-12-02 23:42:26 +0000

    fix up unit tests

--- 2024/1/d1a.py
+++ 2024/1/d1a.py
@@ -1,18 +1,19 @@
 """
-Advent of Code 2024 - Day 1b - 26674158
+Advent of Code 2024 - Day 1a - 1830478
 """
 
 from pathlib import Path
+
+ids_a = []
+ids_b = []
 
 def main(file_name):
     """Main function
 
     :param file_name: The file that contexts the puzzle input
     """
-    ids_a = []
-    ids_b = []
-    answer = 0
 
+    answer = 0
     path = Path(__file__).with_name(file_name)
     with path.open('r', encoding='UTF-8') as file:
         lines = file.readlines()
@@ -21,13 +22,15 @@ def main(file_name):
             ids_a.append(int(row[0]))
             ids_b.append(int(row[1]))
 
-    for content in ids_a:
-        answer += content * (ids_b.count(content))
+    ids_a.sort()
+    ids_b.sort()
 
+    for index, content in enumerate(ids_a):
+        answer += abs(content - ids_b[index])
     return answer
 
-answer_example = main('example.txt')
-answer_input = main('input.txt')
+ANSWER_EXAMPLE = main('example.txt')
+ANSWER_INPUT = main('input.txt')
 
-print(f'Example Input: {answer_example}')
-print(f'Puzzle Input: {answer_input}')
+print(f'Example Input: {ANSWER_EXAMPLE}')
+print(f'Puzzle Input: {ANSWER_INPUT}')
--- 2024/1/test_2024_1.py
+++ 2024/1/test_2024_1.py
@@ -1,8 +1,8 @@
 """Advent of Code 2024 Day 1 Tests
 """
 
-from a import ANSWER_EXAMPLE as a_example, ANSWER_INPUT as a_input
-from b import ANSWER_EXAMPLE as b_example, ANSWER_INPUT as b_input
+from d1a import ANSWER_EXAMPLE as a_example, ANSWER_INPUT as a_input
+from d1b import ANSWER_EXAMPLE as b_example, ANSWER_INPUT as b_input
 
 def test_part_a_example_is_correct():
     """Test to check that part A's example gets the correct answer
