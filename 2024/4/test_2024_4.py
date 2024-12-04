4737cfefdede1db95cef1292ae19e0770412f4bd
zachryall <zach.ryall16@gmail.com>
2024-12-04 20:45:34 +0000

    2024 - Day 4 complete (#8)

---
+++ 2024/4/test_2024_4.py
@@ -0,0 +1,25 @@
+"""Advent of Code 2024 Day 4 Tests
+"""
+
+from y2024_4a import ANSWER_EXAMPLE as a_example, ANSWER_INPUT as a_input
+from y2024_4b import ANSWER_EXAMPLE as b_example, ANSWER_INPUT as b_input
+
+def test_part_a_example_is_correct():
+    """Test to check that part A's example gets the correct answer
+    """
+    assert a_example == 18
+
+def test_part_a_answer_is_correct():
+    """Test to check that part A's input gets the correct answer
+    """
+    assert a_input == 2618
+
+def test_part_b_example_is_correct():
+    """Test to check that part A's example gets the correct answer
+    """
+    assert b_example == 9
+
+def test_part_b_answer_is_correct():
+    """Test to check that part A's input gets the correct answer
+    """
+    assert b_input == 2011
