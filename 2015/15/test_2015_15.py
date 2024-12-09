666e56ff0f98dc0c8719fa4657301fa037959154
zachryall <zach.ryall16@gmail.com>
2024-12-09 00:35:00 +0000

    2015 - Days 15 and 16 complete (#14)

---
+++ 2015/15/test_2015_15.py
@@ -0,0 +1,14 @@
+"""Advent of Code 2015 Day 15 Tests
+"""
+
+from y2015_15 import ANSWER_A, ANSWER_B
+
+def test_part_a_answer_is_correct():
+    """Test to check that part A's input gets the correct answer
+    """
+    assert ANSWER_A == 222870
+
+def test_part_b_answer_is_correct():
+    """Test to check that part B's input gets the correct answer
+    """
+    assert ANSWER_B == 117936
