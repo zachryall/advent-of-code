80525c793a6490b0839da470384ee36adede27d5
zachryall <zach.ryall16@gmail.com>
2024-12-10 21:28:32 +0000

    Day 17 and 18 complete

---
+++ 2015/17/test_2015_17.py
@@ -0,0 +1,14 @@
+"""Advent of Code 2015 Day 17 Tests
+"""
+
+from y2015_17 import ANSWER_A, ANSWER_B
+
+def test_part_a_answer_is_correct():
+    """Test to check that part A's input gets the correct answer
+    """
+    assert ANSWER_A == 1638
+
+def test_part_b_answer_is_correct():
+    """Test to check that part B's input gets the correct answer
+    """
+    assert ANSWER_B == 17
